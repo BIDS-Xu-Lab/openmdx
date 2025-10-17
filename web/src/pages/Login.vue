1484e9bf1cf93961c68e3491fdd13cac5153b7fa
Huan He <140455389+huanhe4096@users.noreply.github.com>
2025-10-16 23:26:42 -0400

    basic login

--- web/src/pages/Login.vue
+++ web/src/pages/Login.vue
@@ -1,29 +1,64 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useUserStore } from '../stores/UserStore';
+import router from '../router';
 
-const userStore = useUserStore();
+const user_store = useUserStore();
 
+const email = ref('');
 const password = ref('');
 const check_remember_me = ref(true);
+
+const onClickBackToHomepage = () => {
+    router.push('/');
+}
+
+async function onClickSignIn() {
+    const result = await user_store.signIn(email.value, password.value);
+    if (result.success) {
+        router.push('/');
+    } else {
+        console.error(result.error);
+    }
+}
 </script>
 <template>
     <div class="bg-surface-50 dark:bg-surface-950 px-6 py-20 md:px-20 lg:px-80">
         <div class="bg-surface-0 dark:bg-surface-900 p-8 md:p-12 shadow-sm rounded-2xl w-full max-w-sm mx-auto flex flex-col gap-8">
+            <div>
+                <a href="./" class="text-xs"
+                    @click="onClickBackToHomepage"
+                    title="Back to Homepage">
+                    &larr;
+                    Back to Homepage
+                </a>
+            </div>
             <div class="flex flex-col items-center gap-4">
                 <div class="flex flex-col items-center gap-2 w-full">
-                    <div class="text-surface-900 dark:text-surface-0 text-2xl font-semibold leading-tight text-center w-full">Welcome Back</div>
+                    <div class="text-surface-900 dark:text-surface-0 text-2xl font-semibold leading-tight text-center w-full">
+                        <font-awesome-icon icon="fa-solid fa-door-open" />
+                        Welcome to OpenMDX
+                    </div>
                 </div>
             </div>
             <div class="flex flex-col gap-6 w-full">
                 <div class="flex flex-col gap-2 w-full">
                     <label for="email1" class="text-surface-900 dark:text-surface-0 font-medium leading-normal">Email Address</label>
-                    <InputText id="email1" type="text" placeholder="Email address" class="w-full px-3 py-2 shadow-sm rounded-lg" />
+                    <InputText id="email1" type="text" 
+                        v-model="email"
+                        placeholder="Email address" 
+                        class="w-full px-3 py-2 shadow-sm rounded-lg" />
                 </div>
                 <div class="flex flex-col gap-2 w-full">
                     <label for="password1" class="text-surface-900 dark:text-surface-0 font-medium leading-normal">Password</label>
 
-                    <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" :feedback="false" input-class="w-full!" />
+                    <Password id="password1" 
+                        v-model="password" 
+                        placeholder="Password" 
+                        :toggleMask="true" 
+                        :feedback="false" 
+                        input-class="w-full!" 
+                        @keyup.enter="onClickSignIn" />
                 </div>
                 <div class="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full gap-3 sm:gap-0">
                     <div class="flex items-center gap-2">
@@ -33,6 +68,7 @@ const check_remember_me = ref(true);
                 </div>
             </div>
             <Button label="Sign In" icon="pi pi-user" 
+                @click="onClickSignIn"
                 class="w-full py-2 rounded-lg flex justify-center items-center gap-2">
                 <template #icon>
                     <i class="pi pi-user text-base! leading-normal!" />
